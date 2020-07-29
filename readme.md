d97396472077ea592962b4cf576744d3f47488d1
Vanderkast <vanderkast@yandex.ru>
2020-07-29 11:13:09 +0300

    keycloak integration init

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # Spring server template
 
 This project is just template that i use to create my Spring services. 
+
+# Keycloak integration
+
+Keycloak integration based on these tutorials and documentations:
+https://medium.com/devops-dudes/securing-spring-boot-rest-apis-with-keycloak-1d760b2004e
+
---
+++ src/main/java/com/template/spring_server/rest/TestRoleController.java
@@ -0,0 +1,27 @@
+package com.template.spring_server.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.RolesAllowed;
+
+@RestController
+@RequestMapping("/api/rolebased")
+public class TestRoleController {
+    @GetMapping("/anon")
+    public @ResponseBody ResponseEntity<String> anon(){
+        return ResponseEntity.ok("Anonymous request");
+    }
+
+    @RolesAllowed("user")
+    @GetMapping("/user")
+    public @ResponseBody ResponseEntity<String> user(){
+        return ResponseEntity.ok("User request");
+    }
+
+    @RolesAllowed("admin")
+    @GetMapping("/admin")
+    public @ResponseBody ResponseEntity<String> admin(){
+        return ResponseEntity.ok("Admin request");
+    }
+}
